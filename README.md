9a487c08301b1da9c9c8cc7a7add3e4277641c4a
yuk7 <yukx00@gmail.com>
2021-06-29 07:34:09 +0900

    uac elevator

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Replace MSGothic Font File Script
 
 ## How-to-Use
-run install.bat in administrator permission
+place modded.ttc and run install.bat in administrator permission
--- install.bat
+++ install.bat
@@ -1,13 +1,25 @@
 :: Copyright (c) 2021 yuk7 <yukx00@gmail.com>
 :: Released under the MIT license
 :: http://opensource.org/licenses/mit-license.php
+
 @echo off
+CD /d %~dp0
+
+net session > nul 2>&1
+if %ERRORLEVEL% neq 0 goto uac
+goto script
+
+:uac
+mshta "javascript: var shell = new ActiveXObject('shell.application'); shell.ShellExecute('%~nx0', '', '', 'runas', 1);close();"
+exit
+
+:script
 cd /d %~dp0
 
 takeown /F %WINDIR%\Fonts\msgothic.ttc /A
 icacls %WINDIR%\Fonts\msgothic.ttc /grant Administrators:F
 move /Y %WINDIR%\Fonts\msgothic.ttc %WINDIR%\Fonts\msgothic.ttc.old
-copy /Y MeiryoKe_Mod.ttc %WINDIR%\Fonts\msgothic.ttc
+copy /Y modded.ttc %WINDIR%\Fonts\msgothic.ttc
 
 echo.
 echo Please Reboot the computer...
